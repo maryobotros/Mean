4a73c360731617394ba184decd302e3f6d4323bc
Maryo Botros <mbotros1912@gmail.com>
2023-09-18 19:34:59 -0700

    Tutorial 22

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
-              <Home/>
+            {/* Home Route */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* Create Route */}
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
